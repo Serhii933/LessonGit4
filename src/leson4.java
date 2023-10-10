804bd3f4c8da8f2a8a3e2937fc248479ecb6495f
Serhii933 <Subota933@gmail.com>
2023-10-10 20:38:26 +0300

    add new system out println

--- src/leson4.java
+++ src/leson4.java
@@ -1,5 +1,6 @@
 public class leson4 {
     public static void main(String[] args){
         System.out.println("Hello world and Git! I am Serhii");
+        System.out.println("seccond commitb to git ");
     }
 }
